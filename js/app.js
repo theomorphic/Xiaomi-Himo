d95196967346b53f428f6f9dc28e2e959f3141ea
theomorphic <FelixMK@yandex.ru>
2022-11-28 00:59:12 +0500

    feat: add some small features

--- js/app.js
+++ js/app.js
@@ -7,4 +7,20 @@ $(function(){
 		autoplay: true,
 		autoplaySpeed: 2500
 	});
+
+	$(".menu, .bike__column").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//ССЫЛКИ В HTML НЕ БУДУТ РАБОТАТЬ, ОНИ ОТКЛЮЧЕНЫ!
+
+		//забираем идентификатор бока с атрибута href
+		let id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 });
